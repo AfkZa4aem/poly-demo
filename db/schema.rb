d0ae5bc9b51e8a15888e4145d43834952668a85b
AfkZa4aem <nastypinagrably@gmail.com>
2017-03-24 11:18:00 +0300

    Add dependencies / apply migration'

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170323180014) do
+ActiveRecord::Schema.define(version: 20170324080356) do
 
   create_table "comments", force: :cascade do |t|
     t.text     "content"
@@ -23,6 +23,12 @@ ActiveRecord::Schema.define(version: 20170323180014) do
 
   create_table "images", force: :cascade do |t|
     t.string   "url"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "links", force: :cascade do |t|
+    t.text     "url"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
